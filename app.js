c708e62ba3905a0baabb55721c3902cecc67d37b
Marcelo Dutto <marcelodutto@hotmail.com>
2022-09-05 22:18:02 -0300

    Se crea la estructura de datos de la app. Incorpora Arrays

--- app.js
+++ app.js
@@ -1,79 +1,54 @@
-//Declaro variables
+//Se crea la estructura de datos con todas las peliculas
 
-let pregunta;
-let respuesta;
-let opcion1;
-let opcion2;
-let opcion3;
-let opcion4;
-let correcta;
-let puntaje = 0;
-
-//Declaro funciones
-
-function generarPregunta(preg, opc1, opc2, opc3, opc4, corr) {
-    pregunta = preg;
-    opcion1 = opc1;
-    opcion2 = opc2;
-    opcion3 = opc3;
-    opcion4 = opc4;
-    correcta = corr;
+function Peliculas(nombre, resumen, estreno, duracion, genero, director, clasificacionEdad, calificacionIMDB) {
+    this.nombre = nombre;
+    this.resumen = resumen;
+    this.estreno =estreno;
+    this.duracion = duracion;
+    this.genero = genero;
+    this.director = director;
+    this.clasificacionEdad = clasificacionEdad;
+    this.calificacionIMDB = calificacionIMDB;
 }
 
-function calcularPuntaje() {
-    if (respuesta === correcta) {
-        puntaje = puntaje + 1;
+const todasPeliculas = [
+    new Peliculas("Matrix", "Un hacker se da cuenta por medio de otros rebeldes de la naturaleza de su realidad y su rol en la guerra contra los controladores.", 1999, 136, "Acción", "Lilly Wachoski/Lana Wachoski", 13, 8.7), 
+    new Peliculas("Interstellar", "Un grupo de exploradores prueban los saltos a través de agujeros de gusano en búsqueda de la sobrevivencia de la humanidad.", 2014, 169, "Ciencia ficción", "Christopher Nolan", 13, 8.6),
+    new Peliculas("The Batman", "Cuando Riddler, un sádico asesino en serie, comienza a asesinar a figuras políticas clave en Gotham, Batman se ve obligado a investigar la corrupción oculta de la ciudad y cuestionar la participación de su familia.", 2022, 176, "Acción", "Matt Reeves", 16, 7.9),
+    new Peliculas("Dune", "Adaptación de la novela de ciencia ficción de Frank Herbert, sobre el hijo de una familia noble encargada de proteger un planeta especiado.", 2021, 155, "Ciencia ficción", "Denis Villeneuve", 13, 8.0),
+    new Peliculas("Tenet", "Un operativo de la CIA es reclutado por una misteriosa organización para realizar una misión de espionaje internacional más allá del tiempo", 2020, 150, "Acción", "Christopher Nolan", 13, 7.3),
+    new Peliculas("El Señor de los Anillos: La comunidad del anillo", "Un Hobbit y ocho compañeros emprendieron un viaje para destruir el poderoso Anillo Único y salvar a la Tierra del Señor Oscuro Sauron.", 2001, 178, "Fantasía", "Peter jackson", 13, 8.8),
+    new Peliculas("El Señor de los Anillos: Las dos torres", "Mientras Frodo y Sam se acercan más a Mordor con la ayuda del cambiante Gollum, la comunidad dividida se opone al nuevo aliado de Sauron, Saruman, y sus hordas de Isengard.", 2002, 179, "Fantasía", "Peter jackson", 13, 8.8),
+    new Peliculas("El Señor de los Anillos: El retorno del Rey", "Gandalf y Aragorn lideran el Mundo de los hombres, e intentarán atraer la atención de Sauron para darle a Frodo la última oportunidad de completar su misión y así tratar de evitar sucumbir a las continuas tentaciones del Anillo.", 2003, 201, "Fantasía", "Peter jackson", 13, 9),
+    new Peliculas("Milagros inesperados", "La vida de los guardias en el corredor de la muerte se ve afectada por uno de los reclusos: un hombre negro acusado de violar y asesinar a un niño, pero que tiene un misterioso don.", 1994, 142, "Drama", "Frank Darabont", 13, 8.6),
+    new Peliculas("Jurassic Park", "Gracias al ADN fosilizado en ámbar, John Hammond da vida a varias especies de dinosaurios y crea Jurassic Park, un parque temático en una isla de Costa Rica. Pero lo que parecía un sueño se convierte rápidamente en pesadilla.", 1993, 127, "Acción", "Steven Spielberg", 13, 8.2),
+    new Peliculas("La lista de Schindler", "Después de presenciar la persecución judía por parte de los nazis en la Polonia ocupada por los alemanes, el empresario Oskar Schindler se preocupa por su fuerza laboral judía.", 1993, 195, "Drama", "Steven Spielberg", 15, 9),
+    new Peliculas("Pulp Fiction", "Cuenta la vida de dos asesinos de la mafia, un boxeador y un pandillero y su esposa, y un par de bandidos se entrelazan en cuatro historias de violencia y redención.", 1994, 144, "Drama", "Quentin Tarantino", 18, 8.9)
+];
+
+//Se declaran funciones
+
+function ingresarUsuario() {
+    cantidad = todasPeliculas.length;
+    nombre = prompt("¡Bienvenido! Por favor, ingrese su nombre");
+    alert(`Hola, ${nombre}. Actualmente contamos con ${cantidad} películas para disfrutar. Presione "aceptar" para ver el catálogo.`)
+}
+
+function mostrarPeliculas() {
+    for(pelicula of todasPeliculas) {
+        alert(` Nombre: ${pelicula.nombre}
+        Descripcion: ${pelicula.resumen}
+        Año de estreno: ${pelicula.estreno}
+        Duración: ${pelicula.duracion}
+        Género: ${pelicula.genero}
+        Director: ${pelicula.director}
+        Clasificacion de edad: ${pelicula.clasificacionEdad}
+        Calificación de IMDB: ${pelicula.calificacionIMDB}`)
     }
 }
 
-// Fin de declaraciones
+//Fin de funciones
 
+ingresarUsuario()
 
-for (let i = 1; i <= 10; i++) {
-    switch (i) {
-        case 1:
-            generarPregunta("¿En qué año se estrenó la primera película de Iron Man, que lanzó el Universo Cinemático de Marvel?", "2005", "2008", "2010", "2012", 2)
-            break;
-        case 2:
-            generarPregunta("¿Cómo se llama el martillo de Thor?", "Vanir", "Mjolnir", "Aesir", "Norn", 2)
-            break;
-        case 3:
-            generarPregunta("¿De qué está hecho el escudo del Capitán América?", "Adamantium", "Vibranio", "Prometeo", "Carbonadio", 2)
-            break;
-        case 4:
-            generarPregunta("¿Cuál es el verdadero nombre de la Pantera Negra?", "T'Challa", "M'Baku", "N'Jadaka", "N'Jobu", 1)
-            break;
-        case 5:
-            generarPregunta("¿Cuál es la raza alienígena que Loki envía para invadir la Tierra en The Avengers?", "Los chitauri", "Los skrulls", "Los kree", "Los flerkens", 1)
-            break;
-        case 6:
-            generarPregunta("¿Quién es asesinado por Loki en los Vengadores?", "Maria Hill", "Nick Fury", "Agente coulson", "Dr. Erik Selvig", 3)
-            break;
-        case 7:
-            generarPregunta("¿Qué tipo de médico es Stephen Strange?", "Neurocirujano", "Cirujano cardiotoracico", "Cirujano de trauma", "Cirujano Plástico", 1)
-            break;
-        case 8:
-            generarPregunta("Antes de convertirse en Visión, ¿cómo se llama el mayordomo de inteligencia artificial de Iron Man?", "Homero", "Alfredo", "Marvin", "Jarvis", 4)
-            break;
-        case 9:
-            generarPregunta("¿Cuál es el verdadero nombre de Black Widow?", "Natalie Rushman", "Natasha Romanoff", "Nicole Rohan", "Naya Rabe", 2)
-            break;
-        case 10:
-            generarPregunta("¿Quién chasquea los dedos en Avengers:Endgame para vencer a Thanos?", "Hulk", "Gamora", "Tony Stark", "Capitana Marvel", 3)
-            break;
-        default:
-            generarPregunta("No es una pregunta valida", "", "", "", "", "");
-            break;
-    }
-
-    respuesta = Number(prompt(i + " - " + `${pregunta}:
-    1 - ${opcion1}
-    2 - ${opcion2}
-    3 - ${opcion3}
-    4 - ${opcion4}`));
-
-   calcularPuntaje();
-}
-
-alert(`Tu puntaje final es ${puntaje}`);
-
+mostrarPeliculas()
